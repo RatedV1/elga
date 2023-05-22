8bd06520b3857c28569ede5f954c9ce3d286f384
RatedV1 <134196607+RatedV1@users.noreply.github.com>
2023-05-22 14:47:10 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./db');
+const cors = require('cors');
+const helmet = require('helmet');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+function connectDB() {
+  MongoClient.connect(process.env.MONGO_URI, (err, client) => {
+    if (err) {
+      console.error('Failed to connect to MongoDB:', err);
+      process.exit(1);
+    }
+    console.log('Connected to MongoDB');
+    // Further code related to your database connection
+  });
+}
+
 connectDB();
+
+app.use(helmet());
+app.use(cors());
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, EGA!');
